1086750f796446987bfbe6e4dad505dc057d0546
王孝元 <243548880@qq.com>
2019-09-02 21:57:32 +0800

    note

--- js/note.js
+++ js/note.js
@@ -1,6 +1,7 @@
-layui.use(['layedit', 'form'], function () {
+layui.use(['layedit', 'form', 'layer'], function () {
     var layedit = layui.layedit;
     var form = layui.form;
+    var layer = layui.layer;
     //建立编辑器
     var index = layedit.build('content', {
         height: 800 //设置编辑器高度
@@ -10,6 +11,7 @@ layui.use(['layedit', 'form'], function () {
         $.ajax({
             type: "POST",
             url: getPort() + "/note/save",
+            contentType: "application/json;charset=utf-8",
             data: JSON.stringify({
                 title: data.field.title,
                 "content": layedit.getContent(index),
@@ -20,6 +22,8 @@ layui.use(['layedit', 'form'], function () {
             success: function (result) {
                 if (result.code == 1) {
                     console.log(result);
+                    layer.msg("保存成功");
+                    category.loadNoteList();
                 } else {
                     alert(result.msg);
                 }
@@ -33,7 +37,8 @@ layui.use(['layedit', 'form'], function () {
         el: "#category",
         data: {
             notes: [],
-            category_id: 0
+            category_id: 0,
+            categories: []
         },
         methods: {
             /**
@@ -56,24 +61,66 @@ layui.use(['layedit', 'form'], function () {
                     error: function (result) {
                     }
                 });
+            },
+            /**
+             * 加载笔记列表
+             */
+            loadNoteList: function () {
+                $.ajax({
+                    type: "GET",
+                    url: getPort() + "/note/findAll/" + category.category_id,
+                    dataType: "json",
+                    headers: {"Token": window.localStorage.token},
+                    success: function (result) {
+                        if (result.code == 1) {
+                            // console.log(result);
+                            category.notes = result.data;
+                        } else {
+                            alert(result.msg);
+                        }
+                    },
+                    error: function (result) {
+                    }
+                });
+            },
+            /**
+             * 加载分类列表
+             */
+            loadCategoryList: function () {
+                $.ajax({
+                    type: "GET",
+                    url: getPort() + "/category/list",
+                    dataType: "json",
+                    headers: {"Token": window.localStorage.token},
+                    success: function (result) {
+                        if (result.code == 1) {
+                            // console.log(result);
+                            category.categories = result.data;
+                        } else {
+                            alert(result.msg);
+                        }
+                    },
+                    error: function (result) {
+                    }
+                });
             }
         },
         mounted: function () {
-            $.ajax({
-                type: "GET",
-                url: getPort() + "/note/findAll/" + this.category_id,
-                dataType: "json",
-                headers: {"Token": window.localStorage.token},
-                success: function (result) {
-                    if (result.code == 1) {
-                        console.log(result);
-                    } else {
-                        alert(result.msg);
-                    }
-                },
-                error: function (result) {
-                }
-            });
+            this.loadNoteList();
         }
     });
+
+    var note = new Vue({
+        el: "#note",
+        data: {},
+        computed: {
+            categories: function () {
+                return category.categories;
+            }
+        },
+        methods: {},
+        mounted: function () {
+        }
+    });
+
 });
