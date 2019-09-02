e8ae3c282b8312475bf07d4b3ea6abac66044fdc
wxy <wangxiaoyuan@pand-auto.com>
2019-09-02 18:42:45 +0800

    保存

--- js/note.js
+++ js/note.js
@@ -2,12 +2,31 @@ layui.use(['layedit', 'form'], function () {
     var layedit = layui.layedit;
     var form = layui.form;
     //建立编辑器
-    layedit.build('content', {
+    var index = layedit.build('content', {
         height: 800 //设置编辑器高度
     });
     //监听提交
-    form.on('submit(formDemo)', function (data) {
-        layer.msg(JSON.stringify(data.field));
+    form.on('submit(saveNote)', function (data) {
+        $.ajax({
+            type: "POST",
+            url: getPort() + "/note/save",
+            data: JSON.stringify({
+                title: data.field.title,
+                "content": layedit.getContent(index),
+                category_id: data.field.category
+            }),
+            dataType: "json",
+            headers: {"Token": window.localStorage.token},
+            success: function (result) {
+                if (result.code == 1) {
+                    console.log(result);
+                } else {
+                    alert(result.msg);
+                }
+            },
+            error: function (result) {
+            }
+        });
         return false;
     });
     var category = new Vue({
