aec617e993ba431c574caab3e27e395ff86025ba
wxy <wangxiaoyuan@pand-auto.com>
2019-09-04 18:11:06 +0800

    remove vue

--- js/index.js
+++ js/index.js
@@ -3,64 +3,29 @@ layui.use(['element', 'layer'], function () {
     var element = layui.element;
     var layer = layui.layer;
 
-    /**
-     * 菜单
-     */
-    var menu = new Vue({
-        el: "#menu",
-        data: {},
-        methods: {
-            goto: function (url) {
-                main.url = url;
-            }
-        },
-        mounted: function () {
-            element.render('menu');
-        }
+    $(function () {
+        get('/user/query/' + window.localStorage.userId, function (result) {
+            $("#username").text(result.data.username);
+        });
     });
+
     /**
-     * iframe
+     * 页面跳转
+     * @param url
      */
-    var main = new Vue({
-        el: "#main",
-        data: {url: "note.html"},
-        methods: {},
-        mounted: function () {
-        }
-    });
+    function jumpTo(url) {
+        $("#main").attr("src", url);
+    }
+
     /**
-     * 头部导航条
+     * 退出
      */
-    var nav = new Vue({
-        el: "#nav",
-        data: {
-            username: ""
-        },
-        methods: {
-            /**
-             * 跳转
-             * @param url
-             */
-            goto: function (url) {
-                main.url = url;
-            },
-            /**
-             * 退出
-             */
-            logout: function () {
-                layer.confirm('确认退出系统吗？', function (index) {
-                    window.localStorage.token = null;// 清除token
-                    window.localStorage.userId = null;// 清除userId
-                    window.location.href = "login.html";
-                    layer.close(index);
-                });
-            }
-        },
-        mounted: function () {
-            get('/user/query/' + window.localStorage.userId, function (result) {
-                nav.username = result.data.username;
-                element.render('nav');
-            });
-        }
-    });
+    function logout() {
+        layer.confirm('确认退出系统吗？', function (index) {
+            window.localStorage.token = null;// 清除token
+            window.localStorage.userId = null;// 清除userId
+            window.location.href = "login.html";
+            layer.close(index);
+        });
+    }
 });
