106b6a516073c2d7d7d789f7457620f129759f86
王孝元 <243548880@qq.com>
2019-09-04 22:16:00 +0800

    note

--- js/note.js
+++ js/note.js
@@ -14,7 +14,29 @@ layui.use(['layedit', 'form', 'layer'], function () {
             category_id: data.field.category
         }, function (result) {
             layer.msg("保存成功", {icon: 1});
+            vue.loadNoteList();
         });
         return false;
+    });
+
+    // 笔记列表
+    var vue = new Vue({
+        el: "#notes",
+        data: {
+            notes: []
+        },
+        methods: {
+            /**
+             * 加载笔记列表
+             */
+            loadNoteList: function () {
+                get('/note/findAll/0', function (result) {
+                    vue.notes = result.data;
+                });
+            }
+        },
+        mounted: function () {
+            this.loadNoteList();
+        }
     });
 });
