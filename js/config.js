9346f68694998b7e9b946675b4af2307d6935fe6
王孝元 <243548880@qq.com>
2019-09-05 22:09:15 +0800

    格式化时间

--- js/config.js
+++ js/config.js
@@ -82,3 +82,30 @@ function del(url, callback) {
         }
     });
 }
+
+/**
+ * 格式化时间
+ * @param UTCDateString
+ * @returns {string}
+ */
+function toLocalTime(UTCDateString) {
+    if (!UTCDateString) {
+        return '-';
+    }
+
+    function formatFunc(str) {    //格式化显示
+        return str > 9 ? str : '0' + str
+    }
+
+    var date2 = new Date(UTCDateString);     //这步是关键
+    var year = date2.getFullYear();
+    var mon = formatFunc(date2.getMonth() + 1);
+    var day = formatFunc(date2.getDate());
+    var hour = date2.getHours();
+    var noon = hour >= 12 ? '下午' : '上午';
+    hour = hour >= 12 ? hour - 12 : hour;
+    hour = formatFunc(hour);
+    var min = formatFunc(date2.getMinutes());
+    var dateStr = year + '-' + mon + '-' + day + ' ' + noon + ' ' + hour + ':' + min;
+    return dateStr;
+}
