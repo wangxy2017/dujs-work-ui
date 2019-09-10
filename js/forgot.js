ad25c713bba12ce765d434c8b150414f5e0fc0d5
wxy <wangxiaoyuan@pand-auto.com>
2019-09-10 10:23:12 +0800

    密码

---
+++ js/forgot.js
@@ -0,0 +1,19 @@
+layui.use(['form', 'layer'], function () {
+    var form = layui.form;
+    var layer = layui.layer;
+    //监听提交
+    form.on('submit(forgot)', function (data) {
+        get('/login/forgot?email=' + data.field.email, function (result) {
+            if (result.code == 1) {
+                layer.alert('新密码已发送至邮箱...', function (index) {
+                    //do something
+                    window.location.href = "login.html";
+                    layer.close(index);
+                });
+            } else {
+                layer.msg(result.msg, {icon: 5});
+            }
+        });
+        return false;
+    });
+});
