9d8f677c9593282e07791da3d4eeee45606caaa8
wxy <wangxiaoyuan@pand-auto.com>
2019-09-10 11:31:19 +0800

    密码

--- js/forgot.js
+++ js/forgot.js
@@ -3,7 +3,7 @@ layui.use(['form', 'layer'], function () {
     var layer = layui.layer;
     //监听提交
     form.on('submit(forgot)', function (data) {
-        get('/login/forgot?email=' + data.field.email, function (result) {
+        get('/login/forgot?email=' + data.field.email + "&username=" + data.field.username, function (result) {
             if (result.code == 1) {
                 layer.alert('新密码已发送至邮箱...', function (index) {
                     //do something
