9135e98437f8f9d101a7afbbe9af65ed24cdb8ae
王孝元 <243548880@qq.com>
2019-09-22 23:14:40 +0800

    意见反馈

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 //JavaScript代码区域
-layui.use(['element', 'layer'], function () {
+layui.use(['element', 'layer', 'form'], function () {
     var element = layui.element;
     var layer = layui.layer;
+    var form = layui.form;
 
+    //监听提交
+    form.on('submit(submitIdea)', function (data) {
+        post('/user/idea',data.field,function (result) {
+            if (result.code == 1) {
+                layer.msg("提交成功", {icon: 6});
+            } else {
+                layer.msg(result.msg, {icon: 5});
+            }
+        });
+        var index = layer.getFrameIndex(window.name);
+        layer.close(index); //关闭弹出层
+        return false;
+    });
     $(function () {
         get('/user/query/' + window.localStorage.getItem("userId"), function (result) {
             if (result.code == 1) {
@@ -17,7 +31,7 @@ layui.use(['element', 'layer'], function () {
      */
     window.jumpTo = function (url) {
         $("#main").attr("src", url);
-    }
+    };
 
     /**
      * 退出
@@ -29,5 +43,16 @@ layui.use(['element', 'layer'], function () {
             window.location.href = "login.html";
             layer.close(index);
         });
-    }
+    };
+    /**
+     * 意见反馈
+     */
+    window.tellIdeas = function () {
+        layer.open({
+            type: 1
+            , title: "意见反馈"
+            , area: ['800px', '500px']
+            , content: $('#giveIdea') //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+        });
+    };
 });
